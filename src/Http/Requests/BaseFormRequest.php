7162f39c385b5c5c3a983ed83d3d8ca8a48e8a69
yassine-dabbous <yassine.dabbous@gmail.com>
2026-02-17 11:04:46 +0000

    BaseModelRequest renamed

--- src/Http/Requests/BaseFormRequest.php
+++ src/Http/Requests/BaseFormRequest.php
@@ -9,12 +9,12 @@ use Illuminate\Support\Facades\Gate;
 use Illuminate\Auth\Access\Response;
 
 /**
- * Class BaseModelRequest
+ * Class BaseFormRequest 
  *
  * A base FormRequest that provides automatic model loading and authorization
  * checks based on Laravel Policies.
  */
-class BaseModelRequest extends FormRequest
+class BaseFormRequest extends FormRequest
 {
     use HasStandardizedErrors;
 
@@ -51,8 +51,14 @@ class BaseModelRequest extends FormRequest
         return $this->model;
     }
 
+    public function getId(): mixed
+    {
+        return $this->route($this->modelRouteKey);
+    }
+
     /**
-     * Query the model from the database using the route key.
+     * Query and return the model instance based on the route parameter.
+     * Override this method in the child class to customize model retrieval.
      */
     public function queryModel(): ?Model
     {
@@ -60,9 +66,7 @@ class BaseModelRequest extends FormRequest
         return $id ? $this->modelClass::find($id) : null;
     }
 
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+    /** Determine if the user is authorized to make this request. */
     public function authorize(): bool
     {
         if (!$this->modelClass) {
@@ -70,7 +74,7 @@ class BaseModelRequest extends FormRequest
         }
 
         $response = match ($this->getMethod()) {
-            'GET' => $this->route($this->modelRouteKey)
+            'GET' => $this->getId()
                 ? Gate::inspect('view', $this->getModel())      // Show single resource
                 : Gate::inspect('viewAny', $this->modelClass),  // Index/list resources
             'POST' => Gate::inspect('create', $this->modelClass),
@@ -85,5 +89,56 @@ class BaseModelRequest extends FormRequest
         }
 
         return true;
+    }
+
+
+    /** Specify keys that should be treated as boolean values */
+    public function booleanKeys(): array
+    {
+        return [];
+    }
+
+    protected function prepareForValidation()
+    {
+        parent::prepareForValidation();
+        
+        // Convert specified keys to boolean values, eg 'true', 'false', '1', '0', etc.
+        foreach ($this->booleanKeys() as $key) {
+            if ($this->has($key)) {
+                $this->merge([
+                    $key => filter_var($this->input($key), FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE),
+                ]);
+            }
+        }
+    }
+    
+    /** Define validation rules for different HTTP methods. */
+    public function rules(): array
+    {
+        return match ($this->getMethod()) {
+            'GET' => $this->searchRules(),
+            'POST' => $this->createRules(),
+            'PUT', 'PATCH' => $this->updateRules(),
+            'DELETE' => [],
+            default => [],
+        };
+    }
+
+    /** Define validation rules for searching (GET requests). */
+    public function searchRules(): array
+    {
+        return [];
+    }
+
+    /** Define validation rules for creating (POST requests). */
+    public function createRules(): array
+    {
+        return [];
+    }
+
+    /** Define validation rules for updating (PUT/PATCH requests). */
+    public function updateRules(): array
+    {
+        return [];
     }
 }
